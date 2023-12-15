fbf74a6c31f8181bb41154502e4298a03ab6652c
DevAhlete <youssef.oumouni@um5r.ac.ma>
2023-12-15 03:01:39 +0100

    Hello using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - entry point.
+ *
+ * description : A function using put to print.
+ *
+ * Return: Always 0 (Success)
+ */
+int mian(void)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+	return(0);
+}
