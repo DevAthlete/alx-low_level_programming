f6344a777d7d133385c7057ac22cb2af72303606
DevAhlete <youssef.oumouni@um5r.ac.ma>
2023-12-21 23:33:13 +0100

    Let's count to 14!

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers
+ */
+
+void more_numbers(void)
+{
+	int r, n, c;
+
+	for (r = 0; r <= 10; r++)
+	{
+	for (n = 0; n <= 14; n++)
+	{
+		c = n;
+		if (n > 9)
+		{
+			_putchar(1 + 48);
+			c = n % 10;
+		}
+		_putchar(c + 48);
+	}
+	putchar('\n');
+	}
+}
