a2c72654332aeaf65d84841d8be952114d6b5ac1
DevAhlete <youssef.oumouni@um5r.ac.ma>
2023-12-21 22:45:11 +0100

    Can you count from 0 to 9?

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ * return: void
+ */
+
+void print_numbers(void)
+{
+	char x;
+
+	for (x = 48; x <= 57; x++)
+		_putchar(x);
+	_putchar('\n');
+}
